688cf8d9614a5f4fbf563c9c086ff2b9f83e5f28
Niklas Knoll <Niklas.Knoll.64@gmail.com>
2020-02-07 12:05:02 +0100

    Design update

--- src/components/userView/history/History.jsx
+++ src/components/userView/history/History.jsx
@@ -18,38 +18,40 @@ const History = ({
             className="accordion__content"
         >
             <p>Hier findest Du alle Deine Urlaubsanträge.</p>
-            {vacations && vacations.map((vaca) => {
-                const startDate = format(parseISO(vaca.startTime), 'dd. MMMM', { locale: de });
+            <div className="list">
+                {vacations && vacations.map((vaca) => {
+                    const startDate = format(parseISO(vaca.startTime), 'dd. MMMM', { locale: de });
 
-                if (isSameDay(parseISO(vaca.startTime), parseISO(vaca.endTime))) {
+                    if (isSameDay(parseISO(vaca.startTime), parseISO(vaca.endTime))) {
+                        return (
+                            <div
+                                key={vaca.startTime}
+                                className="wrapper"
+                            >
+                                <div>{startDate}</div>
+                                {vaca.requestState === 0 ? 'Offen' : vaca.requestState === 1 ? 'Angenommen' : 'Abgelehnt'}
+                            </div>
+                        );
+                    }
+
+                    const endDate = format(parseISO(vaca.endTime), 'dd. MMMM. yyyy', { locale: de });
                     return (
                         <div
-                            key={vaca.startTime}
                             className="wrapper"
+                            key={vaca.startTime}
                         >
-                            <div>{startDate}</div>
+                            <div>
+                                {startDate}
+                                {' '}
+    -
+                                {' '}
+                                {endDate}
+                            </div>
                             {vaca.requestState === 0 ? 'Offen' : vaca.requestState === 1 ? 'Angenommen' : 'Abgelehnt'}
                         </div>
                     );
-                }
-
-                const endDate = format(parseISO(vaca.endTime), 'dd. MMMM. yyyy', { locale: de });
-                return (
-                    <div
-                        className="wrapper"
-                        key={vaca.startTime}
-                    >
-                        <div>
-                            {startDate}
-                            {' '}
--
-                            {' '}
-                            {endDate}
-                        </div>
-                        {vaca.requestState === 0 ? 'Offen' : vaca.requestState === 1 ? 'Angenommen' : 'Abgelehnt'}
-                    </div>
-                );
-            })}
+                })}
+            </div>
         </div>
     </Accordion>
 );
